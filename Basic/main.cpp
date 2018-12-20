fd4338a6c9bad39b7e8f396e0aae0f4040223e41
Jongsoo Jeong <jsjeong@coxlab.kr>
2018-12-20 17:40:24 +0900

    Added a print.

--- Basic/main.cpp
+++ Basic/main.cpp
@@ -8,6 +8,8 @@ static void taskHello(void *) {
 
 void setup() {
   Serial.begin(115200);
+  printf("\n*** [AcSip S76G] Basic ***\n");
+
   timerHello.onFired(taskHello, NULL);
   timerHello.startPeriodic(1000);
 }
